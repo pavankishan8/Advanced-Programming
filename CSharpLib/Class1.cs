481f661dfd0c232cade8c0500ae9a89dc82ca8a7
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-20 14:03:40 +0530

    New

---
+++ CSharpLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLib
+{
+    public class MathComponent
+    {
+        public double AddFunc(double v1, double v2) => v1 + v2;
+        public double SubFunc(double v1, double v2) => v1 - v2;
+        public double MulFunc(double v1, double v2) => v1 * v2;
+        public double DivFunc(double v1, double v2) => v1 / v2;
+        public double SquareRoot(double v1) => Math.Sqrt(v1);
+    }
+}
